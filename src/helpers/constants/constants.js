9bc82538d2edee0ca741902e8c60acec8fd74c3d
apple <adarshsharma002@gmail.com>
2020-04-13 00:21:47 +0530

    update

--- src/helpers/constants/constants.js
+++ src/helpers/constants/constants.js
@@ -1,3 +1,3 @@
 export const  DEX_API_URL = "https://api.haloplatform.tech/dex/";
-export const  DEX_API_URL_TEST = "https://beta-halodex.netlify.com/dex/";
+export const  DEX_API_URL_TEST = "https://halo-backend-test.herokuapp.com/dex/";
 export const  CRYPTO_API_URL = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD&api_key=undefined";
