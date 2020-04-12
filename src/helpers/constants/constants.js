f8908fd24e244de5c7abc6d9a95f3b3a8c075875
apple <adarshsharma002@gmail.com>
2020-04-12 23:40:53 +0530

    update

--- src/helpers/constants/constants.js
+++ src/helpers/constants/constants.js
@@ -1,3 +1,3 @@
 export const  DEX_API_URL = "https://api.haloplatform.tech/dex/";
-export const  DEX_API_URL_TEST = "http://178.62.20.71:3000/dex/";
+export const  DEX_API_URL_TEST = "https://178.62.20.71:3000/dex/";
 export const  CRYPTO_API_URL = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD&api_key=undefined";
